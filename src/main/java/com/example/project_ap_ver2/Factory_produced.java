2fc623b84a12db21f21bf20bc09606d4b4d5d958
Tarush Garg <132291461+Firebred123@users.noreply.github.com>
2024-01-12 21:18:23 +0530

    Delete src/main directory

--- src/main/java/com/example/project_ap_ver2/Factory_produced.java
+++
@@ -1,5 +0,0 @@
-package com.example.project_ap_ver2;
-
-public interface Factory_produced {
-    public void id(String i);
-}
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.project_ap_ver2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires junit;
-
-
-    opens com.example.project_ap_ver2 to javafx.fxml;
-    exports com.example.project_ap_ver2;
-}
